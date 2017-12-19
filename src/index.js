3efec4106b60927584ef4381a669c5ed949c547b
amyShieh <576535019@qq.com>
2017-12-19 21:34:02 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore,applyMiddleware,compose} from 'redux'
-import thunk from 'redux-thunk'
-import {Provider} from 'react-redux'
-import {
-    Switch,
-    Route,
-    BrowserRouter,
-    Redirect
-} from 'react-router-dom'
-import reducers from './reducer/index'
-import App from './App'
-import 'antd/dist/antd.css'
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducers,compose(
-    applyMiddleware(thunk),
-    window.devToolsExtension ?
-    window.devToolsExtension():
-    ()=>{}
-))
-
-ReactDOM.render((
-        <Provider store={store}>
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/login" component={App}></Route>
-                    {/*<App />*/}
-                </Switch>
-            </BrowserRouter>
-        </Provider>
-    ),
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
--- src/reducer/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Amy on 2017/12/15.
- */
-import {combineReducers} from 'redux';
-
-export default combineReducers({});
